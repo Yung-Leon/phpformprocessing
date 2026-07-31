e8bd11146a033fdbd687f42f7d7ce42b1a91245a
Yung-Leon <yungleon542@gmail.com>
2026-07-31 11:15:39 +0300

    Modify students table to enforce NOT NULL and UNIQUE constraints
    Updated the 'students' table schema to make all fields NOT NULL and added a UNIQUE constraint on 'reg_no'.

--- studentmarks/studentapp.sql
+++ studentmarks/studentapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2026 at 12:25 PM
+-- Generation Time: Jul 30, 2026 at 09:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,13 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `students` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `reg_no` varchar(50) DEFAULT NULL,
-  `course` varchar(50) DEFAULT NULL,
-  `total` int(11) DEFAULT NULL,
-  `average` float DEFAULT NULL,
-  `grade` varchar(50) DEFAULT NULL
+  `name` varchar(100) NOT NULL,
+  `reg_no` varchar(50) NOT NULL,
+  `course` varchar(50) NOT NULL,
+  `total` int(11) NOT NULL,
+  `average` float NOT NULL,
+  `grade` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `reg_no`, `course`, `total`, `average`, `grade`) VALUES
+(1, 'Lucas Wen', 'BSCIT26271023', 'IT', 314, 78.5, 'Distinction');
 
 --
 -- Indexes for dumped tables
@@ -45,7 +52,8 @@ CREATE TABLE `students` (
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `reg_no` (`reg_no`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -55,7 +63,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
